495561c91ba2202ddeab27d92681d5f08037539f
Breno-MT <hentgenp@gmail.com>
2023-01-06 21:29:05 -0300

    feat: added Arithmetic Operators

---
+++ Arithmetic_Operators.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+    arithmetic operators
+
+    + (addition)
+    - (subtraction)
+    * (multiplication)
+    / (division)
+    % (modulus)
+    ++ (increment)
+    -- (decrement)
+    */
+
+    int x = 5;
+    int y = 2;
+
+    // float z = x / y;
+    // x++
+    // y--
+    int z = x % y;
+
+    printf("%d", z);
+
+    return 0;
+}
