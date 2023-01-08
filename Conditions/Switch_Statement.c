d0fd493d3496a0af36f020bf95ebd95b36ee6243
Breno-MT <hentgenp@gmail.com>
2023-01-07 22:48:40 -0300

    feat: added Switch Statement

---
+++ Conditions/Switch_Statement.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    // switch = A more efficient alternative to using many "else if" statements
+    // allows a value to be tested for equality against many cases
+
+    char grade;
+
+    printf("\nEnter a letter grade: ");
+    scanf("%c", &grade);
+
+    switch(grade){
+        case 'A':
+            printf("\nPerfect!\n");
+            break;
+        case 'B':
+            printf("\nYou did good!\n");
+            break;
+        case 'C':
+            printf("\nYou did okay.\n");
+            break;
+        case 'D':
+            printf("\nAt least it's not an F!\n");
+            break;
+        case 'F':
+            printf("\nYou Failed! Try harder next time! ;)\n");
+            break;
+        default:
+            printf("\nPlease enter only valid grades! I.e: A,B,C,D,F\n");
+            break;
+    }
+
+    return 0;
+}
