da106b42db3b636606edb1c6bc8e773848675dd4
Breno-MT <hentgenp@gmail.com>
2023-01-08 17:32:40 -0300

    feat: added String Functions

---
+++ Functions/Functions_String.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    //strlwr(string1);      //converts a string to lowercase
+    //strupr(string1);      //converts a string to uppercase
+    //strcat(string1, string2);     //appends string2 to end of string1
+    //strncat(string1, string2, 1);     //appends n characters to string2 to string1
+    //strcpy(string1, string2);     //copy string2 to sintrg1
+    //strncpy(string1, string2, 4);     //copy n characters of string2 to string1
+    //strset(string1, 'x', 1);      //sets all characters of a string to a given character
+    //strnset(string1, 'x', 1);     //sets first n characters of a string to a given character
+    //strrev(string1);      //reverses a string
+    //strlen(string1);      //returns string length as int
+    //strcmp(string1, string1);      //compare all characters
+    //strncmp(string1, string2, 1);     //string compare n characters
+    //strcmpi(string1, string2);        //string compare all (ignore case)
+    //strnicmp(string1, string2, 1);    //string compare n characters (ignore case)
+
+    return 0;
+}
